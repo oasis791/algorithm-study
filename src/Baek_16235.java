9ef9462890730b373ffd8c084d711988314ba714
oasis791 <khwi791@naver.com>
2021-01-10 00:27:43 +0900

    UNSOLVED: Baekjoon_16235

---
+++ src/Baek_16235.java
@@ -0,0 +1,83 @@
+//16235번 나무 재테크
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Baek_16235 {
+    private static int N; // 땅크기
+    private static int M; // 처음 산 나무의 개수
+    private static int K; // 몇년 후
+    private static int year = 0;
+    private static int[][] food;
+    private static ArrayList<Integer>[][] tree;
+    private static int[][] A;
+    private static void spring(){
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                if(tree[i][j].size()!=0){
+                    Collections.sort(tree[i][j]);
+                }
+
+            }
+        }
+    }
+    private static void summer(){
+
+    }
+    private static void autumn(){
+
+    }
+    private static void winter(){
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++)
+                food[i][j] = food[i][j] + A[i][j];
+        }
+    }
+    private static int count(){
+        int count = 0;
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine()," ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        tree = new ArrayList[N+1][N+1];
+        for(int i=0;i<=N;i++){
+            for(int j=0;j<=N;j++)
+                tree[i][j] = new ArrayList<>();
+        }
+        food = new int[N+1][N+1];
+        for(int i=0;i<=N;i++) {
+            Arrays.fill(food[i], 5);
+        }
+        A = new int[N+1][N+1];
+        for(int i=1;i<=N;i++) {
+            st = new StringTokenizer(bf.readLine(), " ");
+            for(int j=1;j<=N;j++)
+                A[i][j] = Integer.parseInt(st.nextToken());
+        }
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(bf.readLine()," ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+            tree[x][y].add(z);
+        }
+        while(year<K){
+            spring();
+            summer();
+            autumn();
+            winter();
+            year++;
+        }
+        System.out.println(count());
+    }
+}
