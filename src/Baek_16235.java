428f6a9577bcb75e701898463fe625d1b244abdf
oasis791 <khwi791@naver.com>
2021-01-10 16:55:50 +0900

    SOLVED: Baekjoon_16235

--- src/Baek_16235.java
+++ src/Baek_16235.java
@@ -9,24 +9,54 @@ public class Baek_16235 {
     private static int M; // 처음 산 나무의 개수
     private static int K; // 몇년 후
     private static int year = 0;
+    private static int[] move_x = {-1,-1,-1,0,0,1,1,1};
+    private static int[] move_y = {-1,0,1,-1,1,-1,0,1};
     private static int[][] food;
     private static ArrayList<Integer>[][] tree;
     private static int[][] A;
-    private static void spring(){
+    private static void springAndSummer(){
         for(int i=1;i<=N;i++){
             for(int j=1;j<=N;j++){
                 if(tree[i][j].size()!=0){
+                    int dead = 0;
                     Collections.sort(tree[i][j]);
+                    for(int k=0;k<tree[i][j].size();k++){
+                        int temp = tree[i][j].get(k);
+                        if(food[i][j]>=temp){
+                            food[i][j] -= temp;
+                            tree[i][j].set(k,temp+1);
+                        }
+                        else {
+                            dead += temp / 2;
+                            tree[i][j].remove(k);
+                            k--;
+                        }
+                    }
+                    food[i][j] += dead;
                 }
-
             }
         }
     }
-    private static void summer(){
-
-    }
     private static void autumn(){
-
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                if(tree[i][j].size()!=0){
+                    for(int k=0;k<tree[i][j].size();k++){
+                        if(tree[i][j].get(k) % 5 == 0){
+                            for(int l=0;l<8;l++){
+                                int nextX = i + move_x[l];
+                                int nextY = j + move_y[l];
+                                if(nextX<1||nextX>N||nextY<1||nextY>N)
+                                    continue;
+                                else{
+                                    tree[nextX][nextY].add(1);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     private static void winter(){
         for(int i=1;i<=N;i++){
@@ -38,7 +68,11 @@ public class Baek_16235 {
         int count = 0;
         for(int i=1;i<=N;i++){
             for(int j=1;j<=N;j++){
-
+                if(tree[i][j].size()!=0) {
+                    for (int k = 0; k < tree[i][j].size(); k++) {
+                        count++;
+                    }
+                }
             }
         }
         return count;
@@ -72,8 +106,7 @@ public class Baek_16235 {
             tree[x][y].add(z);
         }
         while(year<K){
-            spring();
-            summer();
+            springAndSummer();
             autumn();
             winter();
             year++;
