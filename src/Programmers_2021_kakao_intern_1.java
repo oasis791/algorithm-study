56708a7ed51d23d42c02c58e543b4b1d1124c139
oasis791 <khwi791@naver.com>
2022-08-09 00:37:52 +0900

    REFERENCE: reference codes commit

--- src/Programmers_2021_kakao_intern_1.java
+++ src/Programmers_2021_kakao_intern_1.java
@@ -43,7 +43,6 @@ public class Programmers_2021_kakao_intern_1 {
 
     static class Solution {
         public int solution(String s) {
-
             String[]  num= {"0","1","2","3","4","5","6","7","8","9"};
             String[] word= {"zero" , "one" , "two" , "three" , "four" , "five" , "six" , "seven" , "eight" , "nine"};
             for (int i = 0 ; i <10 ; i++){
