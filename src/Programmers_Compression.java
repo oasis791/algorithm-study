e6a6694b1d643ef1a8fea4b397325755bf60a983
oasis791 <khwi791@naver.com>
2022-10-06 01:24:46 +0900

    SOLVED: 압축

---
+++ src/Programmers_Compression.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Programmers_Compression {
+    static class Solution {
+        public static int[] solution(String msg) {
+            Map<String, Integer> dictionary = new HashMap<>();
+            List<Integer> list = new ArrayList<>();
+
+            int index = 1;
+            for (char i = 'A'; i <= 'Z'; i++)
+                dictionary.put(String.valueOf(i), index++);
+
+            String[] split = msg.split("");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < split.length; i++) {
+                sb.delete(0, sb.length());
+                int j = i;
+                for (; j < split.length; j++) {
+                    sb.append(split[j]);
+                    if(!dictionary.containsKey(sb.toString())) {
+                        dictionary.put(sb.toString(), index++);
+                        list.add(dictionary.get(sb.deleteCharAt(sb.length() - 1).toString()));
+                        i = j - 1;
+                        break;
+                    }
+                }
+                if (j == split.length) {
+                    list.add(dictionary.get(sb.toString()));
+                    break;
+                }
+            }
+            return list.stream().mapToInt(i -> i).toArray();
+        }
+    }
+}
