fdf23b4c2e3aa376a78613ecb9b4877d11a8fac3
oasis791 <khwi791@naver.com>
2022-11-22 20:43:08 +0900

    SOLVED: 예상 대진표

---
+++ src/Programmers_Expect_Tournament.java
@@ -0,0 +1,15 @@
+public class Programmers_Expect_Tournament {
+    static class Solution {
+        public static int solution(int n, int a, int b) {
+            int count = 0;
+            while (true) {
+                a = a / 2 + a % 2;
+                b = b / 2 + b % 2;
+                count++;
+                if (a == b)
+                    break;
+            }
+            return count;
+        }
+    }
+}
