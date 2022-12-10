45275707c0b79b6404c74e7b3f8a5cc7ea7e8d78
oasis791 <khwi791@naver.com>
2022-12-10 14:49:55 +0900

    SOLVED: 귤 고르기

---
+++ src/Programmers_Choose_Mandarin.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Programmers_Choose_Mandarin {
+    static class Solution {
+        public static int solution(int k, int[] tangerine) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int j : tangerine) {
+                if (map.containsKey(j)) {
+                    map.put(j, map.get(j) + 1);
+                } else {
+                    map.put(j, 1);
+                }
+            }
+            List<Map.Entry<Integer, Integer>> entryList = new LinkedList<>(map.entrySet());
+            entryList.sort(Map.Entry.comparingByValue((o1, o2) -> o2 - o1));
+
+            int answer = 0;
+            for (int i = 0; i < entryList.size(); i++) {
+                k -= entryList.get(i).getValue();
+                if(k <= 0) {
+                    answer = i + 1;
+                    break;
+                }
+            }
+            return answer;
+        }
+    }
+}
