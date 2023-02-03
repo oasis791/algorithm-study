908189ff8c440073d5c62bc377ebbd81ff4b73d4
oasis791 <khwi791@naver.com>
2023-02-04 01:37:15 +0900

    EDIT: 백준 1916 최소비용 구하기

--- src/Baek_1916.java
+++ src/Baek_1916.java
@@ -1,68 +1,59 @@
-//1916번 최소비용 구하기
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Baek_1916 {
-    private static int N;
-    private static int M;
-    private static int start;
-    private static int end;
-    private static int[] dist;
-    private static ArrayList<Property_1916>[] graph;
-    private static PriorityQueue<Property_1916> pq = new PriorityQueue<>(new Comparator<Property_1916>() {
-        @Override
-        public int compare(Property_1916 o1, Property_1916 o2) {
-            if(o1.weight < o2.weight)
-                return -1;
-            else if(o1.weight > o2.weight)
-                return 1;
-            else
-                return 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        }); // data[0] 에는 노드번호, data[1]에는 가중치
+
+        int N = Integer.parseInt(bf.readLine());
+        int M = Integer.parseInt(bf.readLine());
+        int[][] busInfo = new int[N + 1][N + 1];
+        for (int i = 0; i <= N; i++) {
+            Arrays.fill(busInfo[i], Integer.MAX_VALUE);
         }
-    });
-    static class Property_1916{
-        public int arrive;
-        public int weight;
-        public Property_1916(int v,int w){
-            this.arrive = v;
-            this.weight = w;
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            busInfo[start][end] = Math.min(busInfo[start][end], weight);
         }
-    }
-    private static void dijkstra() {
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int startNode = Integer.parseInt(st.nextToken());
+        int targetNode = Integer.parseInt(st.nextToken());
+
+        int[] distances = new int[N + 1];
+        Arrays.fill(distances, Integer.MAX_VALUE);
+        distances[startNode] = 0;
+        pq.offer(new int[]{startNode, 0});
+
         while (!pq.isEmpty()) {
-            Property_1916 temp = pq.poll();
-            for (Property_1916 i : graph[temp.arrive]) {
-                if (dist[i.arrive] > dist[temp.arrive] + i.weight) {
-                    dist[i.arrive] = dist[temp.arrive] + i.weight;
-                    pq.offer(new Property_1916(i.arrive, dist[i.arrive]));
+            int[] data = pq.poll();
+            int node = data[0];
+            int weight = data[1];
+            if (weight > distances[node])
+                continue;
+            for (int i = 1; i <= N; i++) { // 인접 행렬 탐색
+                if(i == node)
+                    continue;
+                if (busInfo[node][i] != Integer.MAX_VALUE && (weight + busInfo[node][i]) < distances[i]) {
+                    distances[i] = weight + busInfo[node][i];
+                    pq.offer(new int[]{i, distances[i]});
                 }
             }
         }
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bf.readLine());
-        M = Integer.parseInt(bf.readLine());
-        dist = new int[N+1];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        graph = new ArrayList[N+1];
-        for(int i=0;i<=N;i++)
-            graph[i] = new ArrayList<>();
-        for(int i=0;i<M;i++){
-            StringTokenizer st = new StringTokenizer(bf.readLine()," ");
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            graph[u].add(new Property_1916(v,w));
-        }
-        StringTokenizer st = new StringTokenizer(bf.readLine()," ");
-        start = Integer.parseInt(st.nextToken());
-        end = Integer.parseInt(st.nextToken());
-        dist[start] = 0;
-        pq.offer(new Property_1916(start,0));
-        dijkstra();
-        System.out.println(dist[end]);
+        System.out.println(distances[targetNode]);
     }
 }
