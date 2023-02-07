317950d56bc63e21df3bba0e5ee636a9657fdc29
oasis791 <khwi791@naver.com>
2023-02-07 15:50:17 +0900

    SOLVED: 백준 15657 N과 M (8)

---
+++ src/Baek_15657.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baek_15657 {
+    static int N, M;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int arr[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        int index = 0;
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            arr[index++] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            dfs(i, 1, "");
+        }
+
+        br.close();
+        bw.close();
+    }
+
+    static void dfs(int index, int depth, String s) throws IOException {
+        if (depth == M) {
+            s += arr[index] + "\n";
+            bw.write(s);
+            bw.flush();
+            return;
+        }
+
+        s += arr[index] + " ";
+        for (int i = index; i < N; i++) {
+            dfs(i, depth + 1, s);
+        }
+    }
+}
