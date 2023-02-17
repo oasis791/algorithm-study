0a51921ce77067fc0b721059184273ecb8b90b4e
oasis791 <khwi791@naver.com>
2023-02-17 16:16:51 +0900

    EDIT: 백준 1504 특정한 최단 경로

--- src/Baek_1504.java
+++ src/Baek_1504.java
@@ -1,36 +1,81 @@
-//1504번 특정한 최단 경로
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Baek_1504 {
+    static int N, E;
+    static int[][] matrix;
+    static int[] dist;
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        matrix = new int[N + 1][N + 1];
 
-        int N = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-
-        int[][] graph = new int[N + 1][N + 1];
-        int[] dist = new int[N + 1];
 
         for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(bf.readLine(), " ");
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-            graph[start][end] = weight;
+            if (matrix[a][b] != 0) {
+                matrix[a][b] = Math.min(matrix[a][b], c);
+                matrix[b][a] = Math.min(matrix[b][a], c);
+            } else {
+                matrix[a][b] = c;
+                matrix[b][a] = c;
+            }
         }
+        st = new StringTokenizer(br.readLine());
+        int V1 = Integer.parseInt(st.nextToken());
+        int V2 = Integer.parseInt(st.nextToken());
+        dist = new int[N + 1];
 
-        st = new StringTokenizer(bf.readLine(), " ");
-        int[] must = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        // 1. 1 -> V1 -> V2 -> N
+        int answer1 = 0;
+        answer1 += dijkstra(1, V1);
+        answer1 += dijkstra(V1, V2);
+        answer1 += dijkstra(V2, N);
+        // 2. 1 -> V2 -> V1 -> N
+        int answer2 = 0;
+        answer2 += dijkstra(1, V2);
+        answer2 += dijkstra(V2, V1);
+        answer2 += dijkstra(V1, N);
 
-        while (true) {
+        bw.write(String.valueOf((answer1 >= 200000000 && answer2 >= 200000000) ? -1 : Math.min(answer1, answer2)));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 
+    static int dijkstra(int start, int end) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+        Arrays.fill(dist, 200000000);
+        dist[start] = 0;
+
+        pq.offer(new int[]{start, dist[start]});
+        while (!pq.isEmpty()) {
+            int[] data = pq.poll();
+            int node = data[0];
+            int weight = data[1];
+
+            for (int i = 1; i <= N; i++) {
+                if (i != node && matrix[node][i] != 0 && weight + matrix[node][i] < dist[i]) {
+                    dist[i] = weight + matrix[node][i];
+                    pq.offer(new int[]{i, dist[i]});
+                }
+            }
         }
+        return dist[end];
     }
 }
