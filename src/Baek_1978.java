a2204bbcf8a17ade9ae49396ecda9d7e43050f6e
oasis791 <khwi791@naver.com>
2023-02-25 22:52:02 +0900

    SOLVED: 1978 소수 찾기

--- src/Baek_1978.java
+++ src/Baek_1978.java
@@ -6,26 +6,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Baek_1978 {
-    private static int N;
-    private static int result=0;
+    static int result = 0;
+    static boolean[] prime;
+    static int max = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine()," ");
-        N=Integer.parseInt(st.nextToken());
-        int[] input=new int[N];
-        for(int i=0;i<N;i++){
-            st = new StringTokenizer(bf.readLine()," ");
-            input[i]=Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(bf.readLine());
+        int[] input = new int[N];
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, input[i]);
         }
-        for(int i=0;i<N;i++){
-            int check=0;
-            for(int j=2;j<input[i];i++){
-                if(input[i]%j==0)
-                    check++;
-            }
-            if(check==0)
+        prime = new boolean[1000001];
+        isPrime();
+        for (int i = 0; i < N; i++) {
+            if (!prime[input[i]]) {
                 result++;
+            }
         }
+
         System.out.println(result);
     }
+    static void isPrime() {
+        prime[0] = true;
+        prime[1] = true;
+
+        for (int i = 2; i * i < 1000000; i++) {
+            if (!prime[i]) {
+                for (int j = i * i; j <= 1000000; j += i) {
+                    prime[j] = true;
+                }
+            }
+        }
+    }
 }
+
+
