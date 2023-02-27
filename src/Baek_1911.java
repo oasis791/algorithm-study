a4eb8ef34ded720f3e57a6e568764c45ee2f5e36
oasis791 <khwi791@naver.com>
2023-02-27 23:57:41 +0900

    SOLVED: 1911 흙길 보수하기

---
+++ src/Baek_1911.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Baek_1911 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(bf.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0])
+                    return Integer.compare(o1[1], o2[1]);
+                return Integer.compare(o1[0], o2[0]);
+            }
+        });
+
+        int nulpan = 0;
+        int range = 0;
+        for (int i = 0; i < N; i++) {
+            if (arr[i][0] > range)
+                range = arr[i][0];
+            if (arr[i][1] >= range) {
+                while (arr[i][1] > range) {
+                    range += L;
+                    nulpan++;
+                }
+            }
+        }
+
+        System.out.println(nulpan);
+        bf.close();
+    }
+}
