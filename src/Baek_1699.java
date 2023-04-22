7e0190e65d869c145df1893e1668fc975c552398
oasis791 <khwi791@naver.com>
2023-04-23 01:36:21 +0900

    SOLVED: 백준 1699 제곱수의 합

---
+++ src/Baek_1699.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Baek_1699 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
