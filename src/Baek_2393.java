9a89928fbac65e0d5cb290b06e2e26cc3dd29f56
oasis791 <khwi791@naver.com>
2023-08-12 23:05:53 +0900

    SOLVED: 백준 2393 Rook

---
+++ src/Baek_2393.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Baek_2393 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("  ___  ___  ___").append("\n").
+            append("  | |__| |__| |").append("\n").
+            append("  |           |").append("\n").
+            append("   \\_________/").append("\n").
+            append("    \\_______/").append("\n").
+            append("     |     |").append("\n").
+            append("     |     |").append("\n").
+            append("     |     |").append("\n").
+            append("     |     |").append("\n").
+            append("     |_____|").append("\n").
+            append("  __/       \\__").append("\n").
+            append(" /             \\").append("\n").
+            append("/_______________\\").append("\n");
+
+        System.out.println(sb);
+    }
+}
