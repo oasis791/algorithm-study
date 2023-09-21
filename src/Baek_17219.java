b8ac0387260f065bfdfda656054a5c88fcb1a3d1
oasis791 <khwi791@naver.com>
2023-09-21 21:04:29 +0900

    SOLVED: 백준 17219 비밀번호 찾기

---
+++ src/Baek_17219.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Baek_17219 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Map<String, String> info = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(bf.readLine());
+            info.put(st.nextToken(), st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            sb.append(info.get(bf.readLine())).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
