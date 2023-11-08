b1cf32398423f6fb2b9e7ca8b41acb13f3e15767
oasis791 <khwi791@naver.com>
2023-11-08 10:09:38 +0900

    SOLVED: 백준 5339 콜센터

---
+++ src/Baek_5339.java
@@ -0,0 +1,14 @@
+public class Baek_5339 {
+    public static void main(String[] args) {
+        System.out.println("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
