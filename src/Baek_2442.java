2d3890ef37e44f2c3563a54740ca4df401d865ca
oasis791 <khwi791@naver.com>
2023-12-06 23:24:01 +0900

    SOLVED: 백준 2442 별 찍기 - 5

---
+++ src/Baek_2442.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_2442 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for(int j = 0; j < n - i; j++) {
+                sb.append(" ");
+            }
+
+            for(int j = 0; j < 2 * i - 1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
