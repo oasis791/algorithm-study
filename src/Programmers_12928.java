08eb45f7514323cb3f4059a82ac2b5b64834535e
oasis791 <khwi791@naver.com>
2023-12-31 01:53:56 +0900

    SOLVED: 프로그래머스 약수의 합

---
+++ src/Programmers_12928.java
@@ -0,0 +1,17 @@
+public class Programmers_12928 {
+    static class Solution {
+        public int solution(int n) {
+            if (n == 0) return 0;
+
+            int answer = 0;
+
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    answer += i;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
