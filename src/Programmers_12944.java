8020281ce651ce4c487f7d8e2a44e0eb5cc6a813
oasis791 <khwi791@naver.com>
2024-01-15 23:47:55 +0900

    SOLVED: 프로그래머스 평균 구하기

---
+++ src/Programmers_12944.java
@@ -0,0 +1,11 @@
+public class Programmers_12944 {
+    static class Solution {
+        public double solution(int[] arr) {
+            double answer = 0;
+            for(int i = 0; i < arr.length; i++) {
+                answer += arr[i];
+            }
+            return answer / arr.length;
+        }
+    }
+}
