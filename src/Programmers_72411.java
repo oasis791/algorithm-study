7c20e58faf2c1df1cf959a350e82f350f2a6ea96
oasis791 <khwi791@naver.com>
2024-03-04 14:50:50 +0900

    EDIT: 잘못된 코드 수정

--- src/Programmers_72411.java
+++ src/Programmers_72411.java
@@ -3,63 +3,64 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Programmers_72411 {
-    public class Solution {
-        static HashMap<String, Integer> map = new HashMap<>();
-        public String[] solution(String[] orders, int[] course) {
+	static class Solution {
+		static HashMap<String, Integer> map = new HashMap<>();
 
-            ArrayList<String> answer = new ArrayList<>();
+		public String[] solution(String[] orders, int[] course) {
 
-            for (int i = 0; i < orders.length; i++) {
-                String[] orderArr = orders[i].split("");
-                Arrays.sort(orderArr);
-                dfs(orderArr, 0, 0, new StringBuilder());
-            }
+			ArrayList<String> answer = new ArrayList<>();
 
-            ArrayList<String>[] bestMenu = new ArrayList[11];
-            for (int i = 1; i <= 10; i++) {
-                bestMenu[i] = new ArrayList<>();
-            }
-            int[] bestCount = new int[11];
+			for (int i = 0; i < orders.length; i++) {
+				String[] orderArr = orders[i].split("");
+				Arrays.sort(orderArr);
+				dfs(orderArr, 0, 0, new StringBuilder());
+			}
 
-            for (String key : map.keySet()) {
-                int temp = key.length();
-                int val = map.get(key);
+			ArrayList<String>[] bestMenu = new ArrayList[11];
+			for (int i = 1; i <= 10; i++) {
+				bestMenu[i] = new ArrayList<>();
+			}
+			int[] bestCount = new int[11];
 
-                if(val >= 2 && bestCount[temp] < val) {
-                    bestMenu[temp] = new ArrayList<>();
-                    bestMenu[temp].add(key);
-                    bestCount[temp] = val;
-                } else if (val >= 2 && bestCount[temp] == val) {
-                    bestMenu[temp].add(key);
-                }
-            }
+			for (String key : map.keySet()) {
+				int temp = key.length();
+				int val = map.get(key);
 
-            for (int i = 0; i < course.length; i++) {
-                for (int j = 0; j < bestMenu[course[i]].size(); j++) {
-                    answer.add(bestMenu[course[i]].get(j));
-                }
-            }
+				if (val >= 2 && bestCount[temp] < val) {
+					bestMenu[temp] = new ArrayList<>();
+					bestMenu[temp].add(key);
+					bestCount[temp] = val;
+				} else if (val >= 2 && bestCount[temp] == val) {
+					bestMenu[temp].add(key);
+				}
+			}
 
-            String[] arr = new String[answer.size()];
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = answer.get(i);
-            }
+			for (int i = 0; i < course.length; i++) {
+				for (int j = 0; j < bestMenu[course[i]].size(); j++) {
+					answer.add(bestMenu[course[i]].get(j));
+				}
+			}
 
-            Arrays.sort(arr);
-            return arr;
-        }
+			String[] arr = new String[answer.size()];
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = answer.get(i);
+			}
 
-        static void dfs (String[] orderArr, int depth, int index, StringBuilder sb) {
+			Arrays.sort(arr);
+			return arr;
+		}
 
-            if(depth >= 2) {
-                map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
-            }
+		static void dfs(String[] orderArr, int depth, int index, StringBuilder sb) {
 
-            for (int i = index; i < orderArr.length; i++) {
-                sb.append(orderArr[i]);
-                dfs(orderArr, depth + 1, i + 1, sb);
-                sb.deleteCharAt(sb.length() - 1);
-            }
-        }
-    }
+			if (depth >= 2) {
+				map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
+			}
+
+			for (int i = index; i < orderArr.length; i++) {
+				sb.append(orderArr[i]);
+				dfs(orderArr, depth + 1, i + 1, sb);
+				sb.deleteCharAt(sb.length() - 1);
+			}
+		}
+	}
 }
